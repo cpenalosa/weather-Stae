b0a9573e8f9ffb3f936a693a42c1bab33a10f31e
Chris <penalosa619@gmail.com>
2018-02-27 14:22:17 -0500

    Add files via upload

--- temp-humidity-POST.py
+++ temp-humidity-POST.py
@@ -7,60 +7,49 @@ import urllib
 import urllib2
 import requests
 import smbus
-import time
+from datetime import datetime
 
 # Get I2C bus
 bus = smbus.SMBus(1)
 
-# SI7021 address, 0x40(64)
-#		0xF5(245)	Select Relative Humidity NO HOLD master mode
-bus.write_byte(0x40, 0xF5)
-
-time.sleep(0.3)
-
-# SI7021 address, 0x40(64)
-# Read data back, 2 bytes, Humidity MSB first
-data0 = bus.read_byte(0x40)
-data1 = bus.read_byte(0x40)
-
-# Convert the data
-humidity = ((data0 * 256 + data1) * 125 / 65536.0) - 6
-
-time.sleep(0.3)
-
-# SI7021 address, 0x40(64)
-#		0xF3(243)	Select temperature NO HOLD master mode
-bus.write_byte(0x40, 0xF3)
-
-time.sleep(0.3)
-
-# SI7021 address, 0x40(64)
-# Read data back, 2 bytes, Temperature MSB first
-data0 = bus.read_byte(0x40)
-data1 = bus.read_byte(0x40)
-
-# Convert the data
-cTemp = ((data0 * 256 + data1) * 175.72 / 65536.0) - 46.85
-fTemp = cTemp * 1.8 + 32
-
 
 url = 'https://municipal.systems/v1/data?key=8908233f-2995-464c-b984-d46e9768f5df'
+date = datetime.now().strftime('%m-%d %H:%M:%S')
+id = 'tempdemo1' + date
+location = geojson.Point((-87.68724, 42.05673))
 
-id = 'tempdemo1'
-location = geojson.Point(-87.68724, 42.05673)
+if __name__ == '__main__':
+        try:
+                bus.write_byte(0x40, 0xF5)
+                time.sleep(0.3)
+                data0 = bus.read_byte(0x40)
+                data1 = bus.read_byte(0x40)
+                humidity = round(((data0 * 256 + data1) * 125 / 65536.0) - 6, 2)
 
-if result.is_valid():
-	print "Relative Humidity is : %.2f %%" %humidity
-	print "Temperature in Celsius is : %.2f C" %cTemp
-	print "Temperature in Fahrenheit is : %.2f F" %fTemp
+                time.sleep(0.3)
+                
+                bus.write_byte(0x40, 0xF3)
 
-    payload = {'temperature':fTemp, 'humidity':humidity, 'location':location, 'id':id}
+                time.sleep(0.3)
 
-    r = requests.post(url, json=payload) 
-    # params='response=false'
-    print = r.content
-    time.sleep(10)
+                data0 = bus.read_byte(0x40)
+                data1 = bus.read_byte(0x40)
+        
+        # Convert the data
+                cTemp = round(((data0 * 256 + data1) * 175.72 / 65536.0) - 46.85, 2)
+                fTemp = round(cTemp * 1.8 + 32, 2)
+                while True:
+                        print "Relative Humidity is : %.2f %%" %humidity
+                        print "Temperature in Celsius is : %.2f C" %cTemp
+                        print "Temperature in Fahrenheit is : %.2f F" %fTemp
 
-else:
-    print("Error: %d" % result.error_code)
-    time.sleep(10)
+                        payload = {'temperature':fTemp, 'humidity':humidity, 'location':location, 'id':id}
+                        r = requests.post(url, json=payload)
+                        # params='response=false'
+                        print r.content
+                        time.sleep(10)
+                        
+        except (KeyboardInterrupt, SystemExit):
+                        print "\nStopping Weather Station..."
+        print "Done.\nExiting."
+                
